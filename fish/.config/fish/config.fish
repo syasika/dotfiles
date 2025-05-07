e1528fe9da2fddfb9d7a71df146b139bc0091e0e
syasika <shaneyasika82@gmail.com>
2025-05-06 20:49:54 -0400

    fish config changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,6 +13,7 @@ fish_add_path ~/.dotnet/tools
 fish_add_path ~/go/bin
 fish_add_path ~/.local/bin/SonarQube/bin
 fish_add_path ~/local/bin
+fish_add_path ~/openmw49
 fish_greeting ~/local/bin/Tailwind
 
 alias ls='lsd'
@@ -26,7 +27,7 @@ alias projects='cd /mnt/WSDStorage/Shane/Git/'
 #alias tailwindcss='/home/shane/.local/bin/Tailwindcss/tailwindcss-linux-x64'
 alias cls='clear'
 alias pcr='sudo pacman -R $(pacman -Qtdq)'
-
+alias openmw='OpenMW_Launcher-0.49.0_dev_20250424090720_80d6f020ed-x86_64.AppImage'
 set -x MANPAGER 'nvim +Man!'
 
 bass source /usr/share/nvm/init-nvm.sh
